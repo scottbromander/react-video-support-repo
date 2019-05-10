ef40c5416f94ad8e45ad24c38bdb97401addc22b
Scott Bromander <scottbromander@gmail.com>
2019-05-10 16:30:16 -0500

    initial commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const gallery = require('./routes/gallery.router.js');
+const dogs = require('./routes/dog.router');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -10,6 +11,7 @@ app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/gallery', gallery);
+app.use('/dogs', dogs);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
